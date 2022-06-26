806560b9afca1e93819ba8e692e4d301f3228c55
schuhmaj <jonas.schuhmacher@tum.de>
2022-06-26 16:37:38 +0200

    correct version number

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ class CMakeBuild(build_ext):
 # --------------------------------------------------------------------------------
 setup(
     name="polyhedral_gravity",
-    version="1.0.0",
+    version="1.0.1",
     author="Jonas Schuhmacher",
     author_email="jonas.schuhmacher@tum.de",
     description="Package to compute full gravity tensor of a given constant density polyhedron for arbitrary points",
