6f8afe1067cd306e30d72f7d90347ac0e76cf4ca
schuhmaj <jonas.schuhmacher@tum.de>
2022-06-30 16:52:51 +0200

    searching in thrust src dir

--- cmake/thrust.cmake
+++ cmake/thrust.cmake
@@ -50,7 +50,7 @@ unset(_THRUST_VERSION_INCLUDE_DIR CACHE) # Clear old result to force search
 find_path(_THRUST_VERSION_INCLUDE_DIR thrust/version.h
   # NO_DEFAULT_PATH # Only search explicit paths below: COMMENTED BY POLYHEDRAL_GRAVITY_CMAKE
   PATHS
-    \"\${CMAKE_CURRENT_LIST_DIR}/../..\"            # Source tree
+    \"${thrust_SOURCE_DIR}/\"            # Source tree
 )
 set_property(CACHE _THRUST_VERSION_INCLUDE_DIR PROPERTY TYPE INTERNAL)
                     ")
