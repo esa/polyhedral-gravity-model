5127b4d9470dd1c921077799d2f5078e476feb4f
schuhmaj <jonas.schuhmacher@tum.de>
2022-07-05 15:24:02 +0200

    updates to thrust, tetgen set-up

--- cmake/tetgen.cmake
+++ cmake/tetgen.cmake
@@ -2,48 +2,52 @@ include(FetchContent)
 
 message(STATUS "Setting up tetgen")
 
-# IMPORTANT NOTE
-# We do ot use findPackage here, as we modify the one source file slightly to suppress output via stdout!!!
+find_package(tetgen 1.6 QUIET)
 
-#Fetches the version 1.6 for tetgen
-FetchContent_Declare(tetgen
-        GIT_REPOSITORY https://github.com/libigl/tetgen.git
-        GIT_TAG 4f3bfba3997f20aa1f96cfaff604313a8c2c85b6 # release 1.6
-        )
+if (${tetgen_FOUND})
 
-FetchContent_GetProperties(tetgen)
-if(NOT tetgen_POPULATED)
-    FetchContent_Populate(tetgen)
+    message(STATUS "Using local tetgen installation")
 
-    # This is ugly, but functional
-    # We modify one source file in order to prevent console output from the printf function
-    # without polluting the global include path (which would cause to more uglier issues)
-    if(NOT EXISTS ${tetgen_SOURCE_DIR}/tetgen_mod.cxx)
-        message(STATUS "Creating modified tetgen.cxx in order to prevent console output from library")
-        file(READ ${tetgen_SOURCE_DIR}/tetgen.cxx TETGEN_CXX)
-
-        string(REPLACE
-                "#include \"tetgen.h\""
-                "#include \"tetgen.h\"\n#define printf(fmt, ...) (0)\n"
-                TETGEN_CXX "${TETGEN_CXX}")
-
-        file(WRITE ${tetgen_SOURCE_DIR}/tetgen_mod.cxx
-                "${TETGEN_CXX}"
-                )
-    else()
-        message(STATUS "A modified tetgen.cxx already exists! It is assumed that is the correct one disabling output")
-    endif()
-
-    add_library(tetgen STATIC
-            ${tetgen_SOURCE_DIR}/tetgen_mod.cxx
-            ${tetgen_SOURCE_DIR}/predicates.cxx
+else()
+    message(STATUS "Using tetgen from GitHub Source")
+    #Fetches the version 1.6 for tetgen
+    FetchContent_Declare(tetgen
+            GIT_REPOSITORY https://github.com/libigl/tetgen.git
+            GIT_TAG 4f3bfba3997f20aa1f96cfaff604313a8c2c85b6 # release 1.6
             )
 
-    target_compile_definitions(tetgen PRIVATE -DTETLIBRARY)
+    FetchContent_GetProperties(tetgen)
+    if(NOT tetgen_POPULATED)
+        FetchContent_Populate(tetgen)
 
-    target_include_directories(tetgen INTERFACE "${tetgen_SOURCE_DIR}")
+        # This is ugly, but functional
+        # We modify one source file in order to prevent console output from the printf function
+        # without polluting the global include path (which would cause to more uglier issues)
+        if(NOT EXISTS ${tetgen_SOURCE_DIR}/tetgen_mod.cxx)
+            message(STATUS "Creating modified tetgen.cxx in order to prevent console output from library")
+            file(READ ${tetgen_SOURCE_DIR}/tetgen.cxx TETGEN_CXX)
 
+            string(REPLACE
+                    "#include \"tetgen.h\""
+                    "#include \"tetgen.h\"\n#define printf(fmt, ...) (0)\n"
+                    TETGEN_CXX "${TETGEN_CXX}")
+
+            file(WRITE ${tetgen_SOURCE_DIR}/tetgen_mod.cxx
+                    "${TETGEN_CXX}"
+                    )
+        else()
+            message(STATUS "A modified tetgen.cxx already exists! It is assumed that is the correct one disabling output")
+        endif()
+
+        add_library(tetgen
+                ${tetgen_SOURCE_DIR}/tetgen_mod.cxx
+                ${tetgen_SOURCE_DIR}/predicates.cxx
+                )
+
+        target_compile_definitions(tetgen PRIVATE -DTETLIBRARY)
+
+        target_include_directories(tetgen INTERFACE "${tetgen_SOURCE_DIR}")
+    endif()
+    # Disable warnings from the library target
+    target_compile_options(tetgen PRIVATE -w)
 endif()
-
-# Disable warnings from the library target
-target_compile_options(tetgen PRIVATE -w)
--- cmake/thrust.cmake
+++ cmake/thrust.cmake
@@ -24,45 +24,5 @@ else()
             GIT_REPOSITORY https://github.com/NVIDIA/thrust.git
             GIT_TAG 1.16.0
             )
-
-    FetchContent_GetProperties(thrust)
-    if(NOT thrust_POPULATED)
-        # Fetch the content using previously declared details
-        FetchContent_Populate(thrust)
-
-        if(EXISTS ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake)
-            message(STATUS "Found problem causing thrust file (1)")
-            file(READ ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake THRUST_CONFIG_VERSION_FILE)
-            string(REPLACE
-                    "file(READ \"\${_THRUST_VERSION_INCLUDE_DIR}/thrust/version.h\" THRUST_VERSION_HEADER)"
-                    "file(READ \"${thrust_SOURCE_DIR}/thrust/version.h\" THRUST_VERSION_HEADER)"
-                    THRUST_CONFIG_VERSION_FILE "${THRUST_CONFIG_VERSION_FILE}")
-
-            file(WRITE ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake
-                    "${THRUST_CONFIG_VERSION_FILE}"
-                    )
-            message(STATUS "Modified the the problem causing file (1)")
-        else()
-            message(STATUS "Problem causing thrust file (1) not found!")
-        endif()
-
-        if(EXISTS ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake)
-            message(STATUS "Found problem causing thrust file (2)")
-            file(READ ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake THRUST_CONFIG_FILE)
-            string(REPLACE
-                    "target_include_directories(_Thrust_Thrust INTERFACE \"\${_THRUST_INCLUDE_DIR}\")"
-                    "target_include_directories(_Thrust_Thrust INTERFACE \"${thrust_SOURCE_DIR}\")"
-                    THRUST_CONFIG_FILE "${THRUST_CONFIG_FILE}")
-
-            file(WRITE ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake
-                    "${THRUST_CONFIG_FILE}"
-                    )
-            message(STATUS "Modified the the problem causing file (2)")
-        else()
-            message(STATUS "Problem causing thrust file (2) not found!")
-        endif()
-
-        # Bring the populated content into the build
-        add_subdirectory(${thrust_SOURCE_DIR} ${thrust_BINARY_DIR})
-    endif()
+    FetchContent_MakeAvailable(thrust)
 endif()
