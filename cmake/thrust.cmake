f8e0d94f77c1d95d35a9d614e91a075d01d68273
schuhmaj <jonas.schuhmacher@tum.de>
2022-07-05 14:37:44 +0200

    thrust from find_package

--- cmake/thrust.cmake
+++ cmake/thrust.cmake
@@ -2,61 +2,67 @@ include(FetchContent)
 
 message(STATUS "Setting up thrust")
 
-message(STATUS "Using thrust from git repository")
-# Fetches the version 1.16.0 of the official NVIDIA Thrust repository
-FetchContent_Declare(thrust
-        GIT_REPOSITORY https://github.com/NVIDIA/thrust.git
-        GIT_TAG 1.16.0
-        )
+# Set custom variables, policies, etc.
+# Disable stuff not needed
+set(THRUST_ENABLE_HEADER_TESTING "OFF")
+set(THRUST_ENABLE_TESTING "OFF")
+set(THRUST_ENABLE_EXAMPLES "OFF")
 
-FetchContent_GetProperties(thrust)
-if(NOT thrust_POPULATED)
-    # Fetch the content using previously declared details
-    FetchContent_Populate(thrust)
+# Set standard CPP Dialect to 17 (default of thrust would be 14)
+set(THRUST_CPP_DIALECT 17)
 
-    # Set custom variables, policies, etc.
-    # Disable stuff not needed
-    set(THRUST_ENABLE_HEADER_TESTING "OFF")
-    set(THRUST_ENABLE_TESTING "OFF")
-    set(THRUST_ENABLE_EXAMPLES "OFF")
+find_package(thrust 1.16.0 QUIET)
 
-    # Set standard CPP Dialect to 17 (default of thrust would be 14)
-    set(THRUST_CPP_DIALECT 17)
+if (${thrust_FOUND})
 
+    message(STATUS "Using local thrust installation")
 
-    if(EXISTS ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake)
-        message(STATUS "Found problem causing thrust file (1)")
-        file(READ ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake THRUST_CONFIG_VERSION_FILE)
-        string(REPLACE
-                "file(READ \"\${_THRUST_VERSION_INCLUDE_DIR}/thrust/version.h\" THRUST_VERSION_HEADER)"
-                "file(READ \"${thrust_SOURCE_DIR}/thrust/version.h\" THRUST_VERSION_HEADER)"
-                THRUST_CONFIG_VERSION_FILE "${THRUST_CONFIG_VERSION_FILE}")
+else()
+    message(STATUS "Using thrust from git repository")
+    # Fetches the version 1.16.0 of the official NVIDIA Thrust repository
+    FetchContent_Declare(thrust
+            GIT_REPOSITORY https://github.com/NVIDIA/thrust.git
+            GIT_TAG 1.16.0
+            )
 
-        file(WRITE ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake
-                "${THRUST_CONFIG_VERSION_FILE}"
-                )
-        message(STATUS "Modified the the problem causing file (1)")
-    else()
-        message(STATUS "Problem causing thrust file (1) not found!")
+    FetchContent_GetProperties(thrust)
+    if(NOT thrust_POPULATED)
+        # Fetch the content using previously declared details
+        FetchContent_Populate(thrust)
+
+        if(EXISTS ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake)
+            message(STATUS "Found problem causing thrust file (1)")
+            file(READ ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake THRUST_CONFIG_VERSION_FILE)
+            string(REPLACE
+                    "file(READ \"\${_THRUST_VERSION_INCLUDE_DIR}/thrust/version.h\" THRUST_VERSION_HEADER)"
+                    "file(READ \"${thrust_SOURCE_DIR}/thrust/version.h\" THRUST_VERSION_HEADER)"
+                    THRUST_CONFIG_VERSION_FILE "${THRUST_CONFIG_VERSION_FILE}")
+
+            file(WRITE ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config-version.cmake
+                    "${THRUST_CONFIG_VERSION_FILE}"
+                    )
+            message(STATUS "Modified the the problem causing file (1)")
+        else()
+            message(STATUS "Problem causing thrust file (1) not found!")
+        endif()
+
+        if(EXISTS ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake)
+            message(STATUS "Found problem causing thrust file (2)")
+            file(READ ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake THRUST_CONFIG_FILE)
+            string(REPLACE
+                    "target_include_directories(_Thrust_Thrust INTERFACE \"\${_THRUST_INCLUDE_DIR}\")"
+                    "target_include_directories(_Thrust_Thrust INTERFACE \"${thrust_SOURCE_DIR}\")"
+                    THRUST_CONFIG_FILE "${THRUST_CONFIG_FILE}")
+
+            file(WRITE ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake
+                    "${THRUST_CONFIG_FILE}"
+                    )
+            message(STATUS "Modified the the problem causing file (2)")
+        else()
+            message(STATUS "Problem causing thrust file (2) not found!")
+        endif()
+
+        # Bring the populated content into the build
+        add_subdirectory(${thrust_SOURCE_DIR} ${thrust_BINARY_DIR})
     endif()
-
-    if(EXISTS ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake)
-        message(STATUS "Found problem causing thrust file (2)")
-        file(READ ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake THRUST_CONFIG_FILE)
-        string(REPLACE
-                "target_include_directories(_Thrust_Thrust INTERFACE \"\${_THRUST_INCLUDE_DIR}\")"
-                "target_include_directories(_Thrust_Thrust INTERFACE \"${thrust_SOURCE_DIR}\")"
-                THRUST_CONFIG_FILE "${THRUST_CONFIG_FILE}")
-
-        file(WRITE ${thrust_SOURCE_DIR}/thrust/cmake/thrust-config.cmake
-                "${THRUST_CONFIG_FILE}"
-                )
-        message(STATUS "Modified the the problem causing file (2)")
-    else()
-        message(STATUS "Problem causing thrust file (2) not found!")
-    endif()
-
-    # Bring the populated content into the build
-    add_subdirectory(${thrust_SOURCE_DIR} ${thrust_BINARY_DIR})
-
 endif()
