aac9735c44dc11423a5da6ec1db42b9c187a45b8
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2025-09-15 14:57:26 +0200

    update Cibuildhweel setup to include Python 3.14

--- version.cmake
+++ version.cmake
@@ -2,7 +2,7 @@
 get_git_commit_hash(POLYHEDRAL_GRAVITY_COMMIT_HASH)
 is_git_working_tree_clean(POLYHEDRAL_GRAVITY_WORKING_TREE)
 # The Regex of the setup.py captures everything inside the quotes!
-set(POLYHEDRAL_GRAVITY_VERSION "3.3.1rc1")
+set(POLYHEDRAL_GRAVITY_VERSION "3.3.1rc2")
 set(${PROJECT_VERSION} ${POLYHEDRAL_GRAVITY_VERSION})
 
 # Append "-modified" to the commit hash if the working tree is not clean
